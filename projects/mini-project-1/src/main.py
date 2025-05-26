0cf4568cd0e84b0f18c2994dde80a3fab8d724d4
Cernous <redtraitor9@gmail.com>
2025-05-26 09:37:59 -0400

    simple load of the handwritten numbers datasets

--- projects/mini-project-1/src/main.py
+++ projects/mini-project-1/src/main.py
@@ -1,1 +1,8 @@
-print("Hello World")
+from sklearn import datasets
+# import matplotlib.pyplot as plt
+
+digitImg = datasets.load_digits()
+
+# digitImg.data.shape -> (number of images, flattened image pixel count)
+# digitImg.images.shape -> (number of images, width of image, height of image)
+
