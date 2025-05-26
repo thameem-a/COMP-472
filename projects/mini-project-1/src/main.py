cee8c80ca13658686235ea4e0aa1acbd0d912985
Cernous <redtraitor9@gmail.com>
2025-05-26 12:20:19 -0400

    added linear regression example and metrics report

--- projects/mini-project-1/src/main.py
+++ projects/mini-project-1/src/main.py
@@ -1,8 +1,31 @@
 from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+import numpy as np
 # import matplotlib.pyplot as plt
 
+# load digits approx 1.8k 8x8 images
 digitImg = datasets.load_digits()
 
 # digitImg.data.shape -> (number of images, flattened image pixel count)
 # digitImg.images.shape -> (number of images, width of image, height of image)
 
+# https://www.geeksforgeeks.org/understanding-logistic-regression/
+# https://scikit-learn.org/stable/auto_examples/classification/plot_digits_classification.html
+
+# flatten images
+data = digitImg.images.reshape((len(digitImg.images), -1))
+
+# Split data into 50% train and 50% test subsets
+X_train, X_test, y_train, y_test = train_test_split(
+    data, digitImg.target, test_size=0.5, shuffle=False
+)
+
+clf = LogisticRegression(random_state=0).fit(X_train, y_train)
+predicted = clf.predict(X_test)
+
+print(
+    f"Classification report for classifier {clf}:\n"
+    f"{metrics.classification_report(y_test, predicted)}\n"
+)
